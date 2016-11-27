d343dc5bca9f23fa56c87d167af0be9c165c445c
davidtmeadows <dmeadows@stripe.com>
2016-11-27 11:44:19 -0800

    erroneous committing

--- web.rb
+++ web.rb
@@ -57,6 +57,67 @@ post '/purchase' do
     "Charge declined"
   end
 
+<<<<<<< HEAD
+=======
+end 
+
+#endpoint for Stripe Connect Oauth
+get '/connect' do 
+  #gets two params from Stripe OAUTH
+  scope = params[:scope]
+  auth_code = params[:code]
+
+  p scope
+  p auth_code
+
+  #creates account with Stripe or returns error to Sinatra 
+
+  begin
+    response = RestClient.post 'https://connect.stripe.com/oauth/token', {
+      client_secret: $PRIVATE_TEST_KEY,
+      code: auth_code, 
+      grant_type: 'authorization_code'}
+ 
+
+    response = JSON.parse(response.to_s)
+
+    db = SQLite3::Database.new("stripe_store.db")
+    db.execute("INSERT INTO accounts (
+      authorization_code,
+      token_type,
+      stripe_publishable_key,
+      scope,
+      livemode,
+      stripe_user_id,
+      refresh_token,
+      access_token) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [
+      auth_code, 
+      response['token_type'], 
+      response['stripe_publishable_key'], 
+      response['scope'],
+      response['livemode'].to_s,
+      response['stripe_user_id'],
+      response['refresh_token'],
+      response['access_token']
+      ])
+    
+    puts "Account #{response['stripe_user_id']} created." 
+
+    redirect "/account_confirmation?acct_id=#{response['stripe_user_id']}"
+
+  #error handling for restclient and other errors (not really)
+  rescue => e 
+    if e.class.to_s == 'RestClient::BadRequest'
+      e = JSON.parse(e.response.to_s)
+      p e.class
+      "#{e['error']}: #{e['error_description']}"
+    else
+      p e.class
+      p e.to_s
+      p e.cause 
+      p e.message
+      p e.backtrace
+>>>>>>> parent of ea61850... Removed debugging logs
 
 end
 
