17617a47d9a2c56ef7b7545fb086914628e9ba65
davidtmeadows <dmeadows@stripe.com>
2016-11-28 16:47:30 -0800

    cleaned up cart totaling and added dropdown for Checkout button

--- public/magic.js
+++ public/magic.js
@@ -24,14 +24,25 @@ $(document).ready(
 
 	var sumCart = function() {
 		sum = 0;
-  		$( '.product-checks' ).each(function() {
+  		$('.product-checks').each(function() {
   			if ($(this).is(':checked')) {
 
   			sum += parseInt($(this).data("amount"))
-  		}
-  			$('#cart-total').text(sum)
-  			console.log(sum)
-  		});
+  		}	
+  			
+
+  		})
+
+		if (sum > 0) { 
+			$('#cart-total').text("Your cart total is $" + sum/100); 
+			$('#checkout-button').slideDown();
+  			}
+		else {
+			$('#cart-total').text("Cart is currently empty. Add some items!")
+			$('#checkout-button').slideUp();
+  			}
+  		console.log("loggin sum... " + sum)
+  		;
   	}
 
 	$('#cart-total').text(sum)
